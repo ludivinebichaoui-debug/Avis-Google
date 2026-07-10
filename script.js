27d8f06f34c92d1523d4d6cbbcf297d6fa85e3ac
ludivinebichaoui-debug <ludivine_bichaoui@icloud.com>
2026-07-10 16:14:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ function startFlow() {
     // Le navigateur va ouvrir le lien Google Avis dans un nouvel onglet juste après.
     // Ainsi, quand le client aura fini et fermera l'onglet Google, il retombera pile sur la roue déjà affichée !
 }
+function showWheelButton() {
+
+    document.getElementById("waiting").style.display = "block";
+
+    setTimeout(function () {
+
+        document.getElementById("waiting").style.display = "none";
+
+        const btn = document.getElementById("wheelBtn");
+
+        btn.style.display = "inline-block";
+
+        btn.animate([
+            {transform:"scale(0.8)",opacity:0},
+            {transform:"scale(1.08)",opacity:1},
+            {transform:"scale(1)",opacity:1}
+        ],{
+            duration:600
+        });
+
+    },3000);
+
+}
